4e452ac4cc8e270b02addfdf8f3e080824135551
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-03-29 22:07:23 +0900

    Import Excel component

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,17 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import Logo from './components/Logo'
+import Excel from './components/Excel'
 
 const App = () => {
+  const headers = localStorage.getItem('headers') ? localStorage.getItem('headers') : ['タイトル', '年', '評価', 'コメント']
+  const data = localStorage.getItem('data') ? localStorage.getItem('data') : [['テスト', '2015', '3', 'あああ']]
+
   return (
     <div>
       <h1>
         <Logo /> Whinepadにようこそ！
+        <Excel headers={headers} initialData={data} />
       </h1>
     </div>
   )
