0f861db4f8a65cefd856a451a778116c6febf925
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-03-30 20:34:35 +0900

    Add schema file

---
+++ src/js/schema.js
@@ -0,0 +1,35 @@
+import classification from './classification'
+
+export default[
+    {
+        id : 'name',
+        label : '名前',
+        show : true, // Excelに表示するか否か sample: '2ドルのシャック',
+        align : 'left', // Excelでの配置
+    }, {
+        id : 'year',
+        label : '年',
+        type : 'year',
+        show : true,
+        sample : 2015
+    }, {
+        id : 'grape',
+        label : 'ぶどう',
+        type : 'suggest',
+        options : classification.grapes,
+        show : true,
+        sample : 'メルロー ',
+        align : 'left'
+    }, {
+        id : 'rating',
+        label : '評価',
+        type : 'rating',
+        show : true,
+        sample : 3
+    }, {
+        id : 'comments',
+        label : 'コメント',
+        type : 'text',
+        sample : '値段の割にはよい'
+    }
+]
